cffd29c65cdf62f4670800b02ff6958b00d36f57
vyantik <akarizidu2@gmail.com>
2024-04-05 10:28:58 +0300

    gitignore

--- build/utils/event-dto.js
+++
@@ -1,8 +0,0 @@
-class EventDto {
-    constructor(model) {
-        this.id = model.id;
-        this.title = model.title;
-    }
-}
-export default EventDto;
-//# sourceMappingURL=event-dto.js.map
--- build/utils/user-fal.js
+++
@@ -1,8 +0,0 @@
-class UserFal {
-    constructor(model) {
-        this.id = model.id;
-        this.full_name = model.full_name;
-    }
-}
-export default UserFal;
-//# sourceMappingURL=user-fal.js.map
